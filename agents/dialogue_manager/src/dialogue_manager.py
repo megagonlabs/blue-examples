84c17307fd50088c65affaa83e3bd1aec6597171
Jackson Hassell <jackson@ip-10-0-160-75.ec2.internal>
2025-03-04 01:02:39 +0000

    Dialog Manager working prototype

--- agents/dialogue_manager/src/dialogue_manager.py
+++ agents/dialogue_manager/src/dialogue_manager.py
@@ -5,10 +5,11 @@ import json
 from os import write
 
 ###### Blue
-from blue.agent import AgentFactory, AgentRegistry
+from blue.agent import AgentFactory
 from blue.agents.openai import OpenAIAgent
 from blue.session import Session
 from blue.stream import ControlCode
+from blue.plan import Plan
 from blue.utils import string_utils, json_utils, uuid_utils
 
 ##### Agent
@@ -31,103 +32,125 @@ class DialogueManagerAgent(OpenAIAgent):
         if "name" not in kwargs:
             kwargs["name"] = "DIALOGUE_MANAGER"
         super().__init__(**kwargs)
-        self.policy = {
-            "investigate": [
-                ["USER.TEXT", "NL2SQL.DEFAULT"],
-                ["NL2SQL.DEFAULT", "QUERYEXECUTOR.DEFAULT"],
-                ["QUERYEXECUTOR.DEFAULT", self.name + "RESULT"],
-            ],
-            "job_search": [
-                ["USER.TEXT", "OPENAI_EXTRACTOR.DEFAULT"],
-                ["OPENAI_EXTRACTOR.DEFAULT", "NL2SQL.DEFAULT"],
-                ["NL2SQL.DEFAULT", "QUERYEXECUTOR.DEFAULT"],
-                ["QUERYEXECUTOR.DEFAULT", self.name + "RESULT"],
-            ],
-            "summarize": [
-                ["USER.TEXT", "OPENAI_EXTRACTOR.DEFAULT"],
-                ["OPENAI_EXTRACTOR.DEFAULT", "NL2SQL.DEFAULT"],
-                ["NL2SQL.DEFAULT", "QUERYEXECUTOR.DEFAULT"],
-                ["QUERYEXECUTOR.DEDAULT", "SUMMARIZER.DEFAULT"],
-                ["SUMMARIZER.DEFAULT", self.name + "RESULT"],
-            ],
-            "OOD": "Intent not supported",
-        }
+        # self.policy = {
+        #     "investigate": [
+        #         ["USER.TEXT", "NL2SQL.DEFAULT"],
+        #         ["NL2SQL.DEFAULT", "QUERYEXECUTOR.DEFAULT"],
+        #         ["QUERYEXECUTOR.DEFAULT", self.name + "RESULT"],
+        #     ],
+        #     "job_search": [
+        #         ["USER.TEXT", "OPENAI_EXTRACTOR.DEFAULT"],
+        #         ["OPENAI_EXTRACTOR.DEFAULT", "NL2SQL.DEFAULT"],
+        #         ["NL2SQL.DEFAULT", "QUERYEXECUTOR.DEFAULT"],
+        #         ["QUERYEXECUTOR.DEFAULT", self.name + "RESULT"],
+        #     ],
+        #     "summarize": [
+        #         ["USER.TEXT", "OPENAI_EXTRACTOR.DEFAULT"],
+        #         ["OPENAI_EXTRACTOR.DEFAULT", "NL2SQL.DEFAULT"],
+        #         ["NL2SQL.DEFAULT", "QUERYEXECUTOR.DEFAULT"],
+        #         ["QUERYEXECUTOR.DEDAULT", "SUMMARIZER.DEFAULT"],
+        #         ["SUMMARIZER.DEFAULT", self.name + "RESULT"],
+        #     ],
+        #     "OOD": "Intent not supported",
+        # }
 
-    def process_output(self, output_data, properties=None):
-        logging.info("Entered process_output")
-        # get properties, overriding with properties provided
-        properties = self.get_properties(properties=properties)
+    # def process_output(self, output_data, properties=None):
+    #     logging.info("Entered process_output")
+    #     # get properties, overriding with properties provided
+    #     properties = self.get_properties(properties=properties)
 
-        logging.info(output_data)
-        # get gpt plan as json
-        plan = json.loads(output_data)
-        logging.info("Plan:")
-        logging.info(json.dumps(plan, indent=4))
-        logging.info(
-            "========================================================================================================"
-        )
-
-        return plan
+    #     logging.info(output_data)
+    #     # get gpt plan as json
+    #     plan = json.loads(output_data)
+    #     logging.info("Plan:")
+    #     logging.info(json.dumps(plan, indent=4))
+    #     logging.info(
+    #         "========================================================================================================"
+    #     )
+    #     return plan
 
     #### INTENT
-    def identify_intent(self, worker, user_stream, id=None):
-        # create a unique id
-        if id is None:
-            id = util_functions.create_uuid()
+    def identify_intent(self, worker, data, id=None):
+        logging.info('Identifying intent')
 
-        # intent plan
-        intent_plan = [
-            ["USER.TEXT", "OPENAI_CLASSIFIER.DEFAULT"],
-            ["OPENAI_CLASSIFIER.DEFAULT", self.name + ".INTENT"],
-        ]
+        logging.info(str(data))
+        inp = f"\nUser text: {data}.\nPossible intents: {self.properties['intents']}."
 
-        # build query plan
-        plan = util_functions.build_plan(intent_plan, user_stream, id=id)
+        p = Plan(scope=worker.prefix)
+        # set input
+        p.define_input("DEFAULT", value=inp)
+        # set plan
+        p.connect_input_to_agent(from_input="DEFAULT", to_agent='OPENAI___CLASSIFIER')
+        p.connect_agent_to_agent(from_agent='OPENAI___CLASSIFIER', to_agent=self.name, to_agent_input='INTENT')
+        
+        # submit plan
+        p.submit(worker)
 
-        # write plan
-        # TODO: change to tags=["HIDDEN"]
-        util_functions.write_to_new_stream(worker, plan, "PLAN", tags=["PLAN"], id=id)
+        logging.info('Sent off intent classification request')
+        return
+    
+    def build_intent_plan(self, worker, intent):
+        logging.info('Building intent plan')
+        logging.info(intent)
+        p = Plan(scope=worker.prefix)
+        if intent == 'investigate':
+            p.define_input("DEFAULT", value=self.user_input)
+            p.connect_input_to_agent(from_input="DEFAULT", to_agent='NL2SQL___INPLAN')
+            p.connect_agent_to_agent(from_agent='NL2SQL___INPLAN', to_agent='QUERYEXECUTOR', to_agent_input='DEFAULT')
+            p.connect_agent_to_agent(from_agent='QUERYEXECUTOR', to_agent=self.name, to_agent_input='RESULT')
+        elif intent == 'job_search':
+            p.define_input("DEFAULT", value=self.user_input)
+            p.connect_input_to_agent(from_input="DEFAULT", to_agent='OPENAI___EXTRACTOR')
+            p.connect_agent_to_agent(from_agent='OPENAI___EXTRACTOR', to_agent='NL2SQL___INPLAN', to_agent_input='DEFAULT')
+            p.connect_agent_to_agent(from_agent='NL2SQL___INPLAN', to_agent='QUERYEXECUTOR', to_agent_input='DEFAULT')
+            p.connect_agent_to_agent(from_agent='QUERYEXECUTOR', to_agent=self.name, to_agent_input='RESULT')
+        elif intent == 'summarize':
+            p.define_input("DEFAULT", value=self.user_input)
+            p.connect_input_to_agent(from_input="DEFAULT", to_agent='OPENAI___EXTRACTOR')
+            p.connect_agent_to_agent(from_agent='OPENAI___EXTRACTOR', to_agent='NL2SQL___INPLAN', to_agent_input='DEFAULT')
+            p.connect_agent_to_agent(from_agent='NL2SQL___INPLAN', to_agent='QUERYEXECUTOR', to_agent_input='DEFAULT')
+            p.connect_agent_to_agent(from_agent='QUERYEXECUTOR', to_agent='SUMMARIZER', to_agent_input='DEFAULT')
+            p.connect_agent_to_agent(from_agent='SUMMARIZER', to_agent=self.name, to_agent_input='RESULT')
+        else:
+            p.define_input("DEFAULT", value=self.user_input)
+            p.connect_input_to_agent(from_input="DEFAULT", to_agent='OPENAI___ROGUEAGENT')
+            p.connect_agent_to_agent(from_agent='OPENAI___ROGUEAGENT', to_agent=self.name, to_agent_input='RESULT')
 
+        p.submit(worker)
+        logging.info('Submitted intent plan')
         return
 
-    def next_action(self, intent):
-        return self.policy[intent]
+    # def next_action(self, intent):
+    #     return self.policy[intent]
 
     def default_processor(self, message, input="DEFAULT", properties=None, worker=None):
         logging.info("Entered default_processor")
+        logging.info(message)
+        logging.info(input)
         stream = message.getStream()
 
         if input == "DEFAULT":
-
-            if message.isEOS():
-                stream = message.getStream()
-
-                self.identify_intent(worker, stream)
+            if message.isData():
+                data = message.getData()
+                self.user_input = data
+                self.identify_intent(worker, data)
 
         elif input == "INTENT":
             if message.isData():
-                if worker:
-                    data = message.getData()
-                    stream = message.getStream()
-                    logging.info(json.dumps(data, indent=3))
+                data = message.getData()
+                logging.info('Data type')
+                logging.info(type(data))
+                logging.info(str(data))
 
-                    if "intent" in data:
-                        intent = data["intent"]
-                        action = self.next_action(worker, intent)
-                        # TODO: change to check if a plan object
-                        if isinstance(action, list):
-                            plan = util_functions.build_plan(action, stream)
-
-                            util_functions.write_to_new_stream(
-                                worker, plan, "PLAN", tags=["PLAN"]
-                            )
-                        else:
-                            return action
+                intent = json.loads(data)['intent']
+                self.build_intent_plan(worker, intent)
 
         elif input == "RESULT":
             if message.isData():
                 if worker:
                     data = message.getData()
+                    logging.info('In results')
+                    logging.info(data)
                     return data
 
         return None
