352374ce991d4768f1aef61cc233c85dbdf0956e
Eser Kandogan <eser@megagon.ai>
2025-03-10 16:57:02 +0000

    remove agentic employer and job search planner from v0.9

--- agents/agentic_employer/docker_build_agent.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-echo 'Building docker image...'
-echo "${BLUE_CORE_DOCKER_ORG}/blue-agentic_employer:${BLUE_DEPLOY_VERSION}"
-echo "plaforms: ${BLUE_BUILD_PLATFORM}"
-
-# build docker
-docker buildx build --platform ${BLUE_BUILD_PLATFORM} --no-cache --push -t ${BLUE_DEV_DOCKER_ORG}/blue-agentic_employer:${BLUE_DEPLOY_VERSION} -f Dockerfile.agent .
-
-echo 'Done...'
