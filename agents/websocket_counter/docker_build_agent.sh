47b3979557382512380a49313d3176c6419eb86b
Eser Kandogan <eser@megagon.ai>
2025-03-21 23:18:06 +0000

    build args

--- agents/websocket_counter/docker_build_agent.sh
+++ agents/websocket_counter/docker_build_agent.sh
@@ -1,9 +1,9 @@
 #/bin/bash
 echo 'Building docker image...'
-echo "${BLUE_CORE_DOCKER_ORG}/blue-agent-websocket_counter:${BLUE_DEPLOY_VERSION}"
+echo "${BLUE_CORE_DOCKER_ORG}/blue-agent-websocket_counter${BLUE_BUILD_IMG_SUFFIX}:${BLUE_DEPLOY_VERSION}"
 echo "plaforms: ${BLUE_BUILD_PLATFORM}"
 
 # build docker
-docker buildx build --platform ${BLUE_BUILD_PLATFORM} --no-cache --push -t ${BLUE_DEV_DOCKER_ORG}/blue-agent-websocket_counter:${BLUE_DEPLOY_VERSION} -f Dockerfile.agent .
+docker buildx build --platform ${BLUE_BUILD_PLATFORM} ${BLUE_BUILD_CACHE_ARG} --build-arg BLUE_BUILD_CACHE_ARG --build-arg BLUE_BUILD_LIB_ARG --push -t ${BLUE_DEV_DOCKER_ORG}/blue-agent-websocket_counter${BLUE_BUILD_IMG_SUFFIX}:${BLUE_DEPLOY_VERSION} -f Dockerfile.agent .
 
 echo 'Done...'
--- services/websocket_counter/docker_build_service.sh
+++ services/websocket_counter/docker_build_service.sh
@@ -1,9 +1,9 @@
 #/bin/bash
 echo 'Building WebSocketCounter Service...'
-echo "${BLUE_CORE_DOCKER_ORG}/blue-service-websocket_counter:${BLUE_DEPLOY_VERSION}"
+echo "${BLUE_CORE_DOCKER_ORG}/blue-service-websocket_counter${BLUE_BUILD_IMG_SUFFIX}:${BLUE_DEPLOY_VERSION}"
 echo "plaforms: ${BLUE_BUILD_PLATFORM}"
 
 # build docker
-docker buildx build --platform ${BLUE_BUILD_PLATFORM} --no-cache --push -t ${BLUE_DEV_DOCKER_ORG}/blue-service-websocket_counter:${BLUE_DEPLOY_VERSION} -f Dockerfile.service .
+docker buildx build --platform ${BLUE_BUILD_PLATFORM} ${BLUE_BUILD_CACHE_ARG} --build-arg BLUE_BUILD_CACHE_ARG --build-arg BLUE_BUILD_LIB_ARG --push -t ${BLUE_DEV_DOCKER_ORG}/blue-service-websocket_counter${BLUE_BUILD_IMG_SUFFIX}:${BLUE_DEPLOY_VERSION} -f Dockerfile.service .
 
 echo 'Done...'
