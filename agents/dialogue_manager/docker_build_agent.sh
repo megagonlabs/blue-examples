2948f2f08072a097f66109cded5254259bedb76e
Eser Kandogan <eser@megagon.ai>
2025-09-30 18:38:41 +0000

    fix build

--- agents/dialogue_manager/docker_build_agent.sh
+++ agents/dialogue_manager/docker_build_agent.sh
@@ -4,6 +4,6 @@ echo "${BLUE_CORE_DOCKER_ORG}/blue-agent-dialogue_manager${BLUE_BUILD_IMG_SUFFIX
 echo "plaforms: ${BLUE_BUILD_PLATFORM}"
 
 # build docker
-docker buildx build  --platform ${BLUE_BUILD_PLATFORM} ${BLUE_BUILD_CACHE_ARG} --build-arg BLUE_DEPLOY_VERSION --build-arg BLUE_BUILD_CACHE_ARG --build-ARG BLUE_BUILD_LIB_ARG ${BLUE_BUILD_PUBLISH} -t ${BLUE_DEV_DOCKER_ORG}/blue-agent-dialogue_manager${BLUE_BUILD_IMG_SUFFIX}:v${BLUE_DEPLOY_VERSION} -f Dockerfile.agent .
+docker buildx build  --platform ${BLUE_BUILD_PLATFORM} ${BLUE_BUILD_CACHE_ARG} --build-arg BLUE_DEPLOY_VERSION --build-arg BLUE_BUILD_CACHE_ARG --build-arg BLUE_BUILD_LIB_ARG ${BLUE_BUILD_PUBLISH} -t ${BLUE_DEV_DOCKER_ORG}/blue-agent-dialogue_manager${BLUE_BUILD_IMG_SUFFIX}:v${BLUE_DEPLOY_VERSION} -f Dockerfile.agent .
 
 echo 'Done...'
--- tools/basic_calculator/docker_build_tool.sh
+++ tools/basic_calculator/docker_build_tool.sh
@@ -4,7 +4,7 @@ echo "${BLUE_CORE_DOCKER_ORG}/blue-tool-basic_calculator${BLUE_BUILD_IMG_SUFFIX}
 echo "plaforms: ${BLUE_BUILD_PLATFORM}"
 
 # build docker
-docker buildx build  --platform ${BLUE_BUILD_PLATFORM} ${BLUE_BUILD_CACHE_ARG} --build-arg BLUE_BUILD_CACHE_ARG --build-ARG BLUE_BUILD_LIB_ARG
+docker buildx build  --platform ${BLUE_BUILD_PLATFORM} ${BLUE_BUILD_CACHE_ARG} --build-arg BLUE_BUILD_CACHE_ARG --build-arg BLUE_BUILD_LIB_ARG
 ARG BLUE_DEPLOY_VERSION
 ENV BLUE_DEPLOY_VERSION=${BLUE_DEPLOY_VERSION} ${BLUE_BUILD_PUBLISH} -t ${BLUE_DEV_DOCKER_ORG}/blue-tool-basic_calculator${BLUE_BUILD_IMG_SUFFIX}:v${BLUE_DEPLOY_VERSION} -f Dockerfile.tool .
 
