d415da75276aca1198c0d595e84200bcddd652b7
Eser Kandogan <eser@megagon.ai>
2025-09-30 18:12:12 +0000

    dynamic version, build changes, set blue lib to 1.1

--- agents/basic_llm_planner/docker_build_agent.sh
+++ agents/basic_llm_planner/docker_build_agent.sh
@@ -1,9 +1,11 @@
 #/bin/bash
 echo 'Building docker image...'
-echo "${BLUE_CORE_DOCKER_ORG}/blue-agent-basic_llm_planner${BLUE_BUILD_IMG_SUFFIX}:${BLUE_DEPLOY_VERSION}"
+echo "${BLUE_CORE_DOCKER_ORG}/blue-agent-basic_llm_planner${BLUE_BUILD_IMG_SUFFIX}:v${BLUE_DEPLOY_VERSION}"
 echo "plaforms: ${BLUE_BUILD_PLATFORM}"
 
 # build docker
-docker buildx build --platform ${BLUE_BUILD_PLATFORM} ${BLUE_BUILD_CACHE_ARG} --build-arg BLUE_BUILD_CACHE_ARG --build-arg BLUE_BUILD_LIB_ARG -t ${BLUE_DEV_DOCKER_ORG}/blue-agent-basic_llm_planner${BLUE_BUILD_IMG_SUFFIX}:${BLUE_DEPLOY_VERSION} -f Dockerfile.agent .
+docker buildx build  --platform ${BLUE_BUILD_PLATFORM} ${BLUE_BUILD_CACHE_ARG} --build-arg BLUE_DEPLOY_VERSION --build-arg BLUE_BUILD_CACHE_ARG --build-ARG BLUE_BUILD_LIB_ARG
+ARG BLUE_DEPLOY_VERSION
+ENV BLUE_DEPLOY_VERSION=${BLUE_DEPLOY_VERSION} -t ${BLUE_DEV_DOCKER_ORG}/blue-agent-basic_llm_planner${BLUE_BUILD_IMG_SUFFIX}:v${BLUE_DEPLOY_VERSION} -f Dockerfile.agent .
 
 echo 'Done...'
--- tools/basic_calculator/docker_build_tool.sh
+++ tools/basic_calculator/docker_build_tool.sh
@@ -1,9 +1,11 @@
 #/bin/bash
 echo 'Building BasicCalculator Tool...'
-echo "${BLUE_CORE_DOCKER_ORG}/blue-tool-basic_calculator${BLUE_BUILD_IMG_SUFFIX}:${BLUE_DEPLOY_VERSION}"
+echo "${BLUE_CORE_DOCKER_ORG}/blue-tool-basic_calculator${BLUE_BUILD_IMG_SUFFIX}:v${BLUE_DEPLOY_VERSION}"
 echo "plaforms: ${BLUE_BUILD_PLATFORM}"
 
 # build docker
-docker buildx build  --platform ${BLUE_BUILD_PLATFORM} ${BLUE_BUILD_CACHE_ARG} --build-arg BLUE_BUILD_CACHE_ARG --build-arg BLUE_BUILD_LIB_ARG ${BLUE_BUILD_PUBLISH} -t ${BLUE_DEV_DOCKER_ORG}/blue-tool-basic_calculator${BLUE_BUILD_IMG_SUFFIX}:${BLUE_DEPLOY_VERSION} -f Dockerfile.tool .
+docker buildx build  --platform ${BLUE_BUILD_PLATFORM} ${BLUE_BUILD_CACHE_ARG} --build-arg BLUE_BUILD_CACHE_ARG --build-ARG BLUE_BUILD_LIB_ARG
+ARG BLUE_DEPLOY_VERSION
+ENV BLUE_DEPLOY_VERSION=${BLUE_DEPLOY_VERSION} ${BLUE_BUILD_PUBLISH} -t ${BLUE_DEV_DOCKER_ORG}/blue-tool-basic_calculator${BLUE_BUILD_IMG_SUFFIX}:v${BLUE_DEPLOY_VERSION} -f Dockerfile.tool .
 
 echo 'Done...'
